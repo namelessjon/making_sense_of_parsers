7ff3e6aa0d5798fa950bf245cf82f1e22450d7e9
Jon <jonathan.stott@gmail.com>
2019-09-14 18:40:59 +0100

    Fix tests
    Import correct class

--- fifth_test.py
+++ fifth_test.py
@@ -10,9 +10,10 @@ import pytest
                          ("(   1     -  2   )", -1),
                          ("(   1     *  2   )", 2),
                          ("(   1     *  2   )*3*(1+2+3)", 36),
+                         ("1+2*3+4+5-1", 1+2*3+4+5-1)
                          ])
 def test_simple_math_works(test_input, expected):
-    from .fifth import grammar, MathVisitor
-    mv = MathVisitor()
+    from .fifth import grammar, DiceVisitor
+    mv = DiceVisitor()
     parsed = grammar.parse(test_input)
     assert mv.visit(parsed) == expected
