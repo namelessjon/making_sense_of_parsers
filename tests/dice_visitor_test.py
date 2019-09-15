b628bcc3de45de66ae3df5a88dc2a95e549a8e21
Jon <jonathan.stott@gmail.com>
2019-09-15 16:31:50 +0100

    Add tests for dice too

--- tests/dice_visitor_test.py
+++ tests/dice_visitor_test.py
@@ -16,3 +16,25 @@ def test_simple_math_works(test_input, expected):
     from parsimonious_dice.dice_visitor import DiceVisitor
     mv = DiceVisitor()
     assert mv.parse(test_input) == expected
+
+
+def test_dice_works(mocker):
+    mocked_random = mocker.patch('random.randint')
+    mocked_random.return_value = 4  # chosen by fair dice roll. https://xkcd.com/221/
+
+    from parsimonious_dice.dice_visitor import DiceVisitor
+    mv = DiceVisitor()
+    assert mv.parse("2d6+3") == 11
+    assert mocked_random.called_with((1, 6), (1, 6))
+
+
+def test_more_dice_works(mocker):
+    mocked_random = mocker.patch('random.randint')
+    mocked_random.return_value = 4  # chosen by fair dice roll. https://xkcd.com/221/
+
+    from parsimonious_dice.dice_visitor import DiceVisitor
+    mv = DiceVisitor()
+    assert mv.parse("2d6+3*1d4") == 20
+    assert mocked_random.called_with((1, 6), (1, 6), (1, 4))
+
+
