46eb82a28b090ae705962739876cc1b3ebed192f
Jon <jonathan.stott@gmail.com>
2019-09-18 22:01:30 +0100

    Add doc comments for flit

---
+++ parsimonious_dice/__init__.py
@@ -0,0 +1,9 @@
+"""parsimonious_dice implements dice expression parsing via parsimonious
+
+The module can be used as a cli application (including shell), or imported into
+other modules and used there.
+
+It's mostly intended as a learning example, but probably needs more comments
+for that really.  More dice expressions might be added in the future"""
+
+__version__ = "0.1.0"
